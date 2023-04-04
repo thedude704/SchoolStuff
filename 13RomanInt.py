9fdd89381619f50e8621ed1f0c6968fb626582b1
Matt Tyson <thedude704@gmail.com>
2023-04-03 20:43:36 -0400

    Decided to change to tuples as they are immutable vs dictionary which is mutable

--- 13RomanInt.py
+++ 13RomanInt.py
@@ -45,7 +45,7 @@ It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 class Solution:
     def romanToInt(self, s: str) -> int:
         
-        romanNum = {"I" : 1, "V" :5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+        romanNum = ["I" : 1, "V" :5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000]
         
         finalNum = 0
         
